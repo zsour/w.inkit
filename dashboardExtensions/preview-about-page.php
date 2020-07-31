3b16a483d6f7aa43ddfe3441387fa4cc457bdee8
Daniel Karlsson <daniel.karlsson36@outlook.com>
2020-08-01 00:48:57 +0200

    About Image CMS

--- dashboardExtensions/preview-about-page.php
+++ dashboardExtensions/preview-about-page.php
@@ -10,6 +10,7 @@
         <link rel="stylesheet" href="../public/style/index.css">
         <link rel="stylesheet" href="../public/style/products.css">
         <link rel="stylesheet" href="../public/style/terms-and-conditions.css">
+        <link rel="stylesheet" href="../public/style/about-page.css">
     </head>
 
 
@@ -28,7 +29,7 @@
                     </div>
 
 
-                 <div class="terms-and-conditions-content">
+                 <div class="about-page-content">
                     <?php
                          $aboutHeaders = DB::getInstance()->find('about', [
                             'order' => 'order_of_info'
@@ -37,17 +38,31 @@
                             $paragraphArray = json_decode($aboutHeader->info_block);
                      ?>
                      
-                        <div class="terms-and-conditions-content-header">
-                            <a name="<?= $aboutHeader->id; ?>"><?= $aboutHeader->title; ?></a>
+                        <div class="about-content-header">
+                            <p class="about-header-text"><?= $aboutHeader->title; ?></p>
+                            <span class="add-image-icon" id="imgSettingsIcon"></span>
                         </div>
+                        <div class="image-settings-container" id="imgSettingsContainer">
+                            <div class="image-settings-alt-header">Image type:</div>
+                            <select name="imgType" id="imgType">
+                                <option value="wraped" selected>Wraped</option>
+                                <option value="header">Header</option>
+                            </select>                        
+                        </div>
+
+                         <div id="imageContainer">
+                            
+                         </div>
+
+                         <div class="about-paragraph-content">
                             <?php
                                 foreach($paragraphArray as $paragraph):
                             ?>
-                                <div class="terms-and-conditions-content-paragraph"><?= $paragraph; ?></div>
+                                <div class="about-paragraph"><?= $paragraph; ?></div>
                             <?php
                                 endforeach;
                             ?>
-
+                        </div>
                     <?php
                         endforeach;
                     ?>
@@ -56,4 +71,5 @@
     </div>
     </body>
 
+    <script src="../public/js/about-page-image-handler.js"></script>
     </html>
